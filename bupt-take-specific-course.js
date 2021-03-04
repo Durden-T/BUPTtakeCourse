09d95190614f824912e09cc0e4c5d2fe703c702e
Durden-T <1251424057@qq.com>
2021-03-04 21:10:54 +0800

    Update bupt-take-specific-course.js

--- bupt-take-specific-course.js
+++ bupt-take-specific-course.js
@@ -1,4 +1,5 @@
 // https://github.com/Durden-T/BUPTtakeCourse
+
 var courses = [
     //'下一代Internet技术与协议',
     //'体育专项(下)[男]',
@@ -12,56 +13,35 @@ var targets = [];
 var running;
 
 function getCourses() {
-    //必修
-    $.post(
-        '/jsxsd/xsxkkc/xsxkBxxk',
-        {
-            sEcho: 1,
-            iColumns: 11,
-            iDisplayStart: 0,
-            iDisplayLength: 99999,
-        },
-        processData
-    );
-    //选修
-    $.post(
-        '/jsxsd/xsxkkc/xsxkXxxk',
-        {
-            sEcho: 1,
-            iColumns: 11,
-            iDisplayStart: 0,
-            iDisplayLength: 99999,
-        },
-        processData
-    );
-    //公选
-    $.post(
-        '/jsxsd/xsxkkc/xsxkGgxxkxk',
-        {
-            sEcho: 1,
-            iColumns: 11,
-            iDisplayStart: 0,
-            iDisplayLength: 99999,
-        },
-        processData
-    );
+    let params = {
+        sEcho: 1,
+        iColumns: 11,
+        iDisplayStart: 0,
+        iDisplayLength: 99999,
+    };
+    let paths = [
+        '/jsxsd/xsxkkc/xsxkBxxk', //必修
+        '/jsxsd/xsxkkc/xsxkXxxk', //选修
+        '/jsxsd/xsxkkc/xsxkGgxxkxk', //公选
+    ];
+    for (let path of paths)
+        $.post(path, params, processData);
 }
 
 function processData(resp) {
     let data = $.parseJSON(resp).aaData;
-        for (let course of data)
-            if (courses.indexOf(course.kcmc) != -1)
-                targets.push([course.kch, course.jx0404id]);
+    for (let course of data) if (courses.indexOf(course.kcmc) != -1) targets.push([course.kch, course.jx0404id]);
 }
 
 function takeCourses(targets) {
-    if(!targets.length)
-	getCourses();
-    for (let target of targets)
-        $.ajax({
-            url: "/jsxsd/xsxkkc/xxxkOper",
-            data: { kcid: target[0], jx0404id: target[1] }
-        });
+    if (!targets.length) getCourses();
+    for (let target of targets) $.ajax({
+        url: "/jsxsd/xsxkkc/xxxkOper",
+        data: {
+            kcid: target[0],
+            jx0404id: target[1]
+        }
+    });
 
     console.log('running......');
 }
